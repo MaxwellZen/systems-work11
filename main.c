0e014ad098f9b0a932969273d89828f27518675d
Maxwell Zen <maxwell.zen@gmail.com>
2021-10-27 22:58:47 -0400

    better print statements

--- main.c
+++ main.c
@@ -5,26 +5,29 @@
 #include <unistd.h>
 
 int main() {
-	
+
+	printf("Reading numbers...\n\n");
 	int arr[10];
 	int filea = open("/dev/random", O_RDONLY, 0777);
 	read(filea, arr, 10*sizeof(int));
 
 	printf("Original array:\n");
 	for (int i = 0; i < 10; i++) {
-		printf("%d: %d\n", i, arr[i]);
+		printf("\trandom %d: %d\n", i, arr[i]);
 	}
 
+	printf("\nWriting numbers to file...\n\n");
 	int fileb = open("numbers.txt", O_CREAT | O_WRONLY, 0777);
 	write(fileb, arr, 10*sizeof(int));
 
+	printf("Reading numbers from file...\n\n");
 	int filec = open("numbers.txt", O_RDONLY, 0777);
 	int arr2[10];
 	read(filec, arr2, 10*sizeof(int));
 
-	printf("\nCopied array:\n");
+	printf("Copied array:\n");
 	for (int i = 0; i < 10; i++) {
-		printf("%d: %d\n", i, arr2[i]);
+		printf("\trandom %d: %d\n", i, arr2[i]);
 	}
 
 }
